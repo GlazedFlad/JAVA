ced0acd21ff1bab9aa139e63c1fb883038220714
Chase Holland <ch0950163@RK03-B1225-03.francistuttle.edu>
2019-01-18 15:12:58 -0600

    Made P8 use array list instead of arrays

--- src/Chapter07/P7.java
+++ src/Chapter07/P7.java
@@ -1,7 +1,7 @@
 package Chapter07;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * Program to create an array, calculate average and display the newly created
@@ -20,24 +20,27 @@ public class P7 {
         Scanner input = new Scanner(System.in);
         // Create the array
         System.out.print("How many numbers will be read: ");
-        int[] numbers = new int[input.nextInt()];
+        int size = input.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Enter " + size + " elements: ");
 
-        fillArray(numbers, input);
-        System.out.println("The average is " + average(numbers));
-        displayArray(numbers);
+        fillArray(numbers, input, size);
+        System.out.println("The average is " + average(numbers, size));
+        System.out.print("The contents of the array is: ");
+        System.out.println(numbers);
     }
 
     /**
      * collects numbers and stores them in array
      *
      * @param numbers the amount of numbers that the user is going to put into
+     * @param size the size of the array list
      * the array
      * @param input allows for user to input
      */
-    public static void fillArray(int[] numbers, Scanner input) {
-        System.out.print("Enter " + numbers.length + " elements: ");
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = input.nextInt();
+    public static void fillArray(ArrayList<Integer> numbers, Scanner input, int size) {
+        for (int i = 0; i < size; i++) {
+            numbers.add(i, input.nextInt());
         }
     }
 
@@ -45,26 +48,17 @@ public class P7 {
      * calculator for average
      *
      * @param numbers the amount of numbers that the user is going to put into
+     * @param size the size of the array list
      * the array
      * @return calculated average
      */
-    public static double average(int[] numbers) {
+    public static double average(ArrayList<Integer> numbers, int size) {
         double sum = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
+        for (int i = 0; i < size; i++) {
+            sum = sum + numbers.get(i);
         }
 
-        return sum / numbers.length;
-    }
-
-    /**
-     * displays the full array
-     *
-     * @param numbers the amount of numbers that the user is going to put into
-     * the array
-     */
-    public static void displayArray(int[] numbers) {
-        System.out.println("The contents of the array: " + Arrays.toString(numbers));
+        return sum / size;
     }
 }
